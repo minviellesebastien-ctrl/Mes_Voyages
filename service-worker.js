286931b0a821da1bc02b2e3a1be46cec357f3cc8
minviellesebastien-ctrl <minvielle.sebastien@gmail.com>
2026-08-14 18:06:58 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "mes-voyages-v1";
+
+const FILES = [
+    "index.html",
+    "voyages.html",
+    "ajouter.html",
+    "style.css",
+    "style-ajouter.css",
+    "index.js",
+    "voyages.js",
+    "ajouter.js",
+    "fond.png",
+    "fond2.png",
+    "icon-192.png",
+    "icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES);
+        })
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
